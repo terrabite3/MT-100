33e7c5dcde42f37fc722f07f57bd7abe754a964f
Stefan Titus <stefan.titus@keysight.com>
2018-11-20 16:30:13 -0800

    Implement reverb mode, and introduce Property class for easier serialization

--- python/mt_parameters.py
+++ python/mt_parameters.py
@@ -4,6 +4,46 @@ from sysex_memory import *
 from json import JSONEncoder
 
 
+class Property:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.value = None
+
+    def __json__(self, parent):
+        if self.value:
+            parent[self.name] = self.value
+
+    def load_json(self, json):
+        if self.name in json:
+            self.value = json[self.name]
+
+class ChoiceProperty(Property):
+    def __init__(self, name, address, choices):
+        Property.__init__(self, name, address)
+        self.choices = choices
+        print(hex(seven(self.address)))
+
+    def load_from_memory(self, memory):
+        print(hex(seven(self.address)))
+        if self.address in memory:
+            raw_value = memory[self.address]
+            if raw_value >= len(self.choices) or raw_value < 0:
+                raise RuntimeError('Invalid index {} while loading {} from memory.'.format(raw_value, self.name))
+            self.value = self.choices[raw_value]
+
+    def write_to_memory(self, memory):
+        if self.value:
+            try:
+                index = self.choices.index(self.value)
+                print(hex(seven(self.address)))
+                print(index)
+                memory[self.address] = index
+            except ValueError as e:
+                raise RuntimeError('Invalid choice {} while writing {} to memory.'.format(self.value, self.name))
+
+
+
 class MtParameters:
     CHANNEL_TIMBRE_TEMP =   native(0x02_00_00)
     PATCH_TEMP =            native(0x03_00_00)
@@ -72,14 +112,18 @@ class MtParameters:
 
 class System:
 
-    MASTER_TUNE = native(0x00)
+    MASTER_TUNE = 0x00
     MASTER_TUNE_MIN = 432.1
     MASTER_TUNE_MAX = 457.6
+    
 
     def __init__(self, offset):
         self.offset = offset
 
         self.master_tune = None
+        
+        self.reverb_mode = ChoiceProperty('reverb_mode', 0x01, ['Room', 'Hall', 'Plate', 'Tap delay'])
+        self.reverb_mode.address += offset
 
     def load_from_memory(self, memory):
         if self.offset + self.MASTER_TUNE in memory:
@@ -87,6 +131,8 @@ class System:
             f_min = self.MASTER_TUNE_MIN
             f_max = self.MASTER_TUNE_MAX
             self.master_tune = f_min + (f_max - f_min) * (raw_value / 127)
+
+        self.reverb_mode.load_from_memory(memory)
 
     def write_to_memory(self, memory):
         if self.master_tune:
@@ -98,17 +144,24 @@ class System:
             raw_value = max(raw_value, 0)
             memory[self.offset + self.MASTER_TUNE] = raw_value
 
+        self.reverb_mode.write_to_memory(memory)
+
+
     def __json__(self):
         result = {}
         
         if self.master_tune:
             result['master_tune'] = self.master_tune
 
+        self.reverb_mode.__json__(result)
+
         return result
 
     def load_json(self, json):
         if 'master_tune' in json:
             self.master_tune = json['master_tune']
+
+        self.reverb_mode.load_json(json)
 
 
 
