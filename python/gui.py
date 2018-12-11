34b27b5629ba90fb8e045e726443c40818ed173e
Stefan Titus <stefan.titus@keysight.com>
2018-12-11 12:28:35 -0800

    Use rebind to simplify binding

--- python/gui.py
+++ python/gui.py
@@ -3,22 +3,32 @@ kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, NumericProperty
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.spinner import Spinner
 
 import mt_parameters as mt
 
 parameters = mt.MtParameters()
 
+class ParamSpinner(Spinner):
+    param = ObjectProperty(mt.ChoiceProperty('', 0, ['']), rebind=True)
 
+class IntSliderTextBox(GridLayout):
+    param = ObjectProperty(mt.IntProperty('', 0), rebind=True)
+    value = NumericProperty()
 
 class SystemLayout(GridLayout):
+    params = ObjectProperty(mt.MtParameters(), rebind=True)
+
+class MainLayout(TabbedPanel):
     params = ObjectProperty()
 
 class MTApp(App):
 
 
     def build(self):
-        return SystemLayout(params=parameters)
+        return MainLayout(params=parameters)
 
 
 if __name__ == '__main__':
