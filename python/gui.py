3e4c16b7103266bfdb0a575d55d558e608904521
Stefan Titus <stefan.titus@keysight.com>
2018-12-17 14:41:57 -0800

    Experiment some more with GUI architecture - UNSTABLE

--- python/gui.py
+++ python/gui.py
@@ -8,8 +8,34 @@ from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.spinner import Spinner
 
 import mt_parameters as mt
+from sysex_memory import *
+
+import sys
+import os
 
 parameters = mt.MtParameters()
+
+if len(sys.argv) > 1:
+    in_file = sys.argv[1]
+    in_extension = os.path.splitext(in_file)[1].lower()
+    
+    memory = SysExMemory()
+
+    if in_extension == '.json':
+        with open(in_file, 'r') as json_file:
+            in_dict = json.load(json_file)
+
+        parameters.load_dict(in_dict)
+
+
+    elif in_extension == '.syx':
+        memory.read_syx(in_file)
+        parameters.load_from_memory(memory.memory)
+
+
+    elif in_extension == '.mem':
+        memory.read_memory(in_file)
+        parameters.load_from_memory(memory.memory)
 
 class ParamSpinner(Spinner):
     param = ObjectProperty(mt.ChoiceProperty('', 0, ['']), rebind=True)
@@ -18,8 +44,17 @@ class IntSliderTextBox(GridLayout):
     param = ObjectProperty(mt.IntProperty('', 0), rebind=True)
     value = NumericProperty()
 
+class ParamTextInput(TextInput):
+    param = ObjectProperty
+
 class SystemLayout(GridLayout):
     params = ObjectProperty(mt.MtParameters(), rebind=True)
+
+class PitchEnvelope(GridLayout):
+    params = ObjectProperty(mt.PitchEnvelope(0), rebind=True)
+
+# class TimbreLayout(GridLayout):
+#     params = ObjectProperty(mt.MtParameters(), rebind=True)
 
 class MainLayout(TabbedPanel):
     params = ObjectProperty()
